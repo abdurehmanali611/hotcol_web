51e2566f02a8620072fc0bfa85fcba2b0964f404
abdurehmanali611 <abdurehmanali611@gmail.com>
2025-12-19 21:12:39 +0300

    First

---
+++ Components/CreateCredential.tsx
@@ -0,0 +1,10 @@
+
+const CreateCredential = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+        
+    </div>
+  )
+}
+
+export default CreateCredential
